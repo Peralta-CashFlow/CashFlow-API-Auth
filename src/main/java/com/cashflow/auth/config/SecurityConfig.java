ba2438d6b876b3b3a0f5d5227c7db7191a30475c
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-03-24 16:17:35 -0300

    feat: spring security configuration

--- src/main/java/com/cashflow/auth/config/SecurityConfig.java
+++ src/main/java/com/cashflow/auth/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.cashflow.auth.config;
 
+import com.cashflow.auth.core.filter.JwtValidatorFilter;
+import com.cashflow.auth.core.service.jwt.CashFlowJwtService;
+import com.cashflow.auth.filter.CashFlowLoginFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,26 +11,49 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    @Value(value = "${jwt.secret}")
+    private String jwtSecret;
+
+    private static final String[] SECURITY_WHITELIST_ENDPOINT = {
+            "/auth/user/register",
+            "/auth/user/login",
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html"
+    };
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public CashFlowJwtService cashFlowJwtService() {
+        return new CashFlowJwtService(jwtSecret);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity,
+                                            CashFlowJwtService cashFlowJwtService,
+                                            CashFlowLoginFilter cashFlowLoginFilter
+    ) throws Exception {
         return httpSecurity
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request ->
-                        request.requestMatchers("/auth/user/register").permitAll()
-                                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        request.requestMatchers(SECURITY_WHITELIST_ENDPOINT).permitAll()
                                 .anyRequest().authenticated()
                 )
+                .addFilterBefore(cashFlowLoginFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtValidatorFilter(cashFlowJwtService, SECURITY_WHITELIST_ENDPOINT), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
--- src/main/java/com/cashflow/auth/service/user/IUserService.java
+++ src/main/java/com/cashflow/auth/service/user/IUserService.java
@@ -2,11 +2,16 @@ package com.cashflow.auth.service.user;
 
 import com.cashflow.auth.domain.dto.request.UserCreationRequest;
 import com.cashflow.auth.domain.dto.response.UserResponse;
+import com.cashflow.auth.domain.entities.User;
 import com.cashflow.commons.core.dto.request.BaseRequest;
 import com.cashflow.exception.core.CashFlowException;
+
+import java.util.Locale;
 
 public interface IUserService {
 
     UserResponse register(BaseRequest<UserCreationRequest> baseRequest) throws CashFlowException;
+
+    User findUserByEmailAndPassword(String email, String password, Locale locale) throws CashFlowException;
 
 }
