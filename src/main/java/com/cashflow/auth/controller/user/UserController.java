f706c35d7a4c134f9e4b423158697abb72ff3b3e
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-03-24 16:16:53 -0300

    feat: signup endpoint

--- src/main/java/com/cashflow/auth/controller/user/UserController.java
+++ src/main/java/com/cashflow/auth/controller/user/UserController.java
@@ -1,5 +1,6 @@
 package com.cashflow.auth.controller.user;
 
+import com.cashflow.auth.core.domain.authentication.CashFlowAuthentication;
 import com.cashflow.auth.domain.dto.request.UserCreationRequest;
 import com.cashflow.auth.domain.dto.response.UserResponse;
 import com.cashflow.auth.service.user.IUserService;
@@ -14,8 +15,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -60,6 +63,32 @@ public class UserController {
     ) throws CashFlowException {
         log.info("Received request to register a new user with e-mail: {}", userCreationRequest.email());
         return userService.register(new BaseRequest<>(language, userCreationRequest));
+    }
+
+    @Operation(
+            summary = "Login a user",
+            description = "Should login a user from the provided request data."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User logged successfully",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = CashFlowAuthentication.class))
+            ),
+            @ApiResponse(responseCode = "401", description = "Email or password invalid",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class))
+            ),
+            @ApiResponse(responseCode = "500", description = "Error while generation JWT token",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class))
+            )
+    })
+    @GetMapping("/login")
+    public CashFlowAuthentication login(
+            @Parameter(name = "email", description = "User e-mail", required = true) @RequestParam @NotEmpty String email,
+            @Parameter(name = "password", description = "User password", required = true) @RequestParam @NotEmpty String password,
+            @Parameter(name = "Accept-Language", description = "Language to be used on response messages", example = "pt")
+            @RequestHeader(name = "Accept-Language", required = false, defaultValue = "en") Locale language
+    ) {
+        log.info("User successfully logged in via CashFlowLoginFilter with e-mail: {}", email);
+        return (CashFlowAuthentication) SecurityContextHolder.getContext().getAuthentication();
     }
 
 }
