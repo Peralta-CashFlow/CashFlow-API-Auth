2d66f2d069b9ca0facf92089748f2d2e05e0f6e9
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-03-24 22:06:27 -0300

    test: controller login test

--- src/test/java/com/cashflow/auth/controller/user/UserControllerTest.java
+++ src/test/java/com/cashflow/auth/controller/user/UserControllerTest.java
@@ -1,7 +1,9 @@
 package com.cashflow.auth.controller.user;
 
 import com.cashflow.auth.config.BaseTest;
+import com.cashflow.auth.core.domain.authentication.CashFlowAuthentication;
 import com.cashflow.auth.domain.dto.response.UserResponse;
+import com.cashflow.auth.domain.templates.dto.CashFlowAuthenticationTemplates;
 import com.cashflow.auth.domain.templates.entities.UserTemplates;
 import com.cashflow.auth.repository.profile.ProfileRepository;
 import com.cashflow.auth.repository.user.UserRepository;
@@ -12,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -61,5 +64,18 @@ class UserControllerTest extends BaseTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(userResponse)));
 
+    }
+
+    @Test
+    @SneakyThrows
+    void givenEmailAndPassword_whenCashFlowAuthentication_thenReturnCashFlowAuthentication() {
+        CashFlowAuthentication cashFlowAuthentication = CashFlowAuthenticationTemplates.getCashFlowAuthentication();
+        SecurityContextHolder.getContext().setAuthentication(cashFlowAuthentication);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(BASE_REQUEST_URL + "/login")
+                        .param("email", "vinicius-peralta@hotmail.com")
+                        .param("password", "123456"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(cashFlowAuthentication)));
     }
 }
