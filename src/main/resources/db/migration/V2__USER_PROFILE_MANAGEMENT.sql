1945b0092a26a73850a7a4c51cb33a13b07022ec
ViniciusPeralta <vinicius-peralta@hotmail.com>
2025-07-01 22:33:20 -0300

    build: update user table

--- src/main/resources/db/migration/V2__USER_PROFILE_MANAGEMENT.sql
+++ src/main/resources/db/migration/V2__USER_PROFILE_MANAGEMENT.sql
@@ -1,3 +1,9 @@
 -- Delete column account_type from tb_user table
 ALTER TABLE tb_user DROP COLUMN account_type;
 
+-- Add new columns to tb_user table
+ALTER TABLE tb_user
+ADD COLUMN avatar LONGBLOB NULL,
+ADD COLUMN gender VARCHAR(1) NULL,
+ADD COLUMN birthday DATE NULL,
+ADD COLUMN tax_number VARCHAR(14) NULL;
